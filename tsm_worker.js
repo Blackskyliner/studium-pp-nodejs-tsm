b445faf5d29d693929215f9c9c4f000fdab5d50c
Blackskyliner <blackskyliner@googlemail.com>
2013-12-20 23:05:01 +0100

    Added another TSP example

--- tsm_worker.js
+++ tsm_worker.js
@@ -571,6 +571,32 @@ EOF"
 //*/
 
 /*
+var Network = GN.createNetworkFromTSP(
+"NAME: burma14\n\
+TYPE: TSP\n\
+COMMENT: 14-Staedte in Burma (Zaw Win)\n\
+DIMENSION: 14\n\
+EDGE_WEIGHT_TYPE: GEO\n\
+EDGE_WEIGHT_FORMAT: FUNCTION\n\
+DISPLAY_DATA_TYPE: COORD_DISPLAY\n\
+NODE_COORD_SECTION\n\
+1 16.47 96.10\n\
+2 16.47 94.44\n\
+3 20.09 92.54\n\
+4 22.39 93.37\n\
+5 25.23 97.24\n\
+6 22.00 96.05\n\
+7 20.47 97.02\n\
+8 17.20 96.29\n\
+9 16.30 97.38\n\
+10 14.05 98.12\n\
+11 16.53 97.38\n\
+12 21.52 95.59\n\
+13 19.41 97.13\n\
+14 20.09 94.55\n\
+EOF");
+//*/
+
 var startTime = microtime.nowStruct();
 
 var suite = new Benchmark.Suite;
